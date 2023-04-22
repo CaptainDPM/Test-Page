71b17ff9f808197b04e3c167ae7469aaea84dcc5
CaptainDPM <100872653+CaptainDPM@users.noreply.github.com>
2023-04-21 19:28:30 -1000

    Added React

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-alert ("Hello everyone!");
+alert ("Let's add!");
+let firstNum = parseInt(prompt ("Please pick a number."));
+let secNum = parseInt(prompt ("Please pick a second number."));
+let result = firstNum + secNum;
+alert (result);
